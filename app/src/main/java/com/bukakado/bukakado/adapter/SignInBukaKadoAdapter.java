b9b379d11519eb37ae32a9d474fb27e486e31a44
jessicacasanovalim <jessicacasanovalim@yahoo.co.id>
2017-05-21 02:39:43 +0700

    save user token to firebase user

--- app/src/main/java/com/bukakado/bukakado/adapter/SignInBukaKadoAdapter.java
+++ app/src/main/java/com/bukakado/bukakado/adapter/SignInBukaKadoAdapter.java
@@ -15,8 +15,13 @@ import com.bukakado.bukakado.helper.RestClient;
 import com.bukakado.bukakado.interfaces.AdapterCallback;
 import com.bukakado.bukakado.interfaces.LoginInterface;
 import com.bukakado.bukakado.model.response.BukalapakLoginResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -27,6 +32,10 @@ import retrofit2.Response;
  */
 
 public class SignInBukaKadoAdapter extends RecyclerView.Adapter<SignInBukaKadoAdapter.ViewHolder> {
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref = database.getReference("users");
+    String userUID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    HashMap<String, Object> resultMap = new HashMap<>();
 
     int size=0;
     public SignInBukaKadoAdapter()
@@ -67,7 +76,7 @@ public class SignInBukaKadoAdapter extends RecyclerView.Adapter<SignInBukaKadoAd
                 String userName = txtUsername.getText().toString();
                 String password = txtPassword.getText().toString();
 
-                LoginInterface result = RestClient.createService(LoginInterface.class,userName,password);
+                final LoginInterface result = RestClient.createService(LoginInterface.class,userName,password);
                 Call<BukalapakLoginResponse> responseCall=result.getAccessToken();
                 responseCall.enqueue(new Callback<BukalapakLoginResponse>() {
                     @Override
@@ -79,6 +88,8 @@ public class SignInBukaKadoAdapter extends RecyclerView.Adapter<SignInBukaKadoAd
                             txtError.setText(bukalapakLoginResponse.getMessage());
                         }
                         else {
+                            resultMap.put("userToken", bukalapakLoginResponse.getToken());
+                            ref.child(userUID).updateChildren(resultMap);
                             txtError.setText("success login"+bukalapakLoginResponse.getToken());
                         }
                     }
